842f22698958b3c3243cd0f1edf69bee7d1f7059
sylvqinZ <szanini@live.fr>
2022-06-28 21:13:59 +0200

    product page & catch script

--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,10 @@
 fetch("http://localhost:3000/api/products")
     .then((res) => res.json())
-    .then((data) => addProducts(data))
+    .then((products) => addProducts(products))
+    .catch((err) => {
+        console.log("error")
+        console.log(err);
+});
     
 
     //altTxt: "Photo d'un canapé bleu, deux places"
@@ -12,7 +16,7 @@ fetch("http://localhost:3000/api/products")
     //_id: "107fb5b75607497b96722bda5b504926"
 
 
-function addProducts(sofas) {
+function addProducts(sofas = '') {
     //const id = data[0]._id
     //const imageUrl = data[0].imageUrl
     //const altTxt = data[0].altTxt
@@ -20,10 +24,8 @@ function addProducts(sofas) {
     //const description = data[0].description
    
     sofas.forEach((sofa) => {
-    
-        
-        const { id, imageUrl, altTxt, name, description } = sofa
-        const anchor = makeAnchor(id)
+        const { _id, imageUrl, altTxt, name, description } = sofa
+        const anchor = makeAnchor(_id)
         const article = document.createElement("article")
         const image = makeImage(imageUrl, altTxt)
         const h3 = makeH3(name)
@@ -32,48 +34,48 @@ function addProducts(sofas) {
         appendElementsToArticle(article, image, h3, p)
         appendArticleToAnchor(anchor, article)
     })
-    
-}
+};
 
-function appendElementsToArticle(article, image, h3, p) {
+function appendElementsToArticle(article = '', image= '', h3 = '', p = '') {
     article.appendChild(image)
     article.appendChild(h3)
     article.appendChild(p)
-}
+};
 
-function makeAnchor(id) {
+function makeAnchor(id = '') {
     const anchor = document.createElement("a")
     anchor.href = "./product.html?id=" + id
     return anchor
-}
+};
 
-function appendArticleToAnchor(anchor, article) {
+function appendArticleToAnchor(anchor = '', article = '') {
     const items = document.getElementById("items")
     if (items != null){
         items.appendChild(anchor)
         anchor.appendChild(article)
        
     } 
-}
+};
 
-function makeImage(imageUrl, altTxt){
+function makeImage(imageUrl = '', altTxt = ''){
     const image = document.createElement("img")
     image.src = imageUrl
     image.alt = altTxt
     return image
-}
+};
 
-function makeH3(name) {
+function makeH3(name = '') {
     const h3 = document.createElement("h3")
     h3.textContent = name
     h3.classList.add("productName")
     return h3
-}
+};
 
-function makeParagraph(description){
+function makeParagraph(description = ''){
     const p = document.createElement("p")
     p.textContent = description
     p.classList.add("productDescription")
     return p
-}
+};
+
 
